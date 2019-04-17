be14415669e2409f4e995581f209e08266e425b6
EdersonLuiz <49735185+EdersonLuiz@users.noreply.github.com>
2019-04-17 19:51:23 -0300

    Add files via upload

---
+++ Exercicio7.py
@@ -0,0 +1,8 @@
+'Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32 '
+
+celsius = int (input ("Digite a temperatura atual:"))
+
+fahrenheit = celsius / 5 + 32
+
+print ('valor em C: ', celsius)
+print ('valor em F: ', fahrenheit)
